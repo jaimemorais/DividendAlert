5245a77fc8eb939157defb7f9fd6076896526bd4
jaimemorais <jaimemorais@gmail.com>
2018-03-30 16:42:24 -0300

    First Commit

---
+++ DividendAlert/DividentAlertConfigs.cs
@@ -0,0 +1,9 @@
+namespace DividendAlert
+{
+    public class DividendAlertConfigs
+    {
+        public string UrlDividendPage { get; set; }
+        public string StockList { get; set; }
+        public int Timeout { get; set; }
+    }
+}
