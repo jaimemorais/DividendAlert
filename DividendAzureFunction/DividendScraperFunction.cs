9614e40e3df39a9937da58394f158bcbdb49cef5
Jaime Morais <jaimemorais@gmail.com>
2019-11-19 14:04:57 -0300

    Add const

--- DividendAzureFunction/DividendScraperFunction.cs
+++ DividendAzureFunction/DividendScraperFunction.cs
@@ -10,25 +10,23 @@ namespace DividendAzureFunction
 {
     public static class DividendScraperFunction
     {
+        ////"https://www.bussoladoinvestidor.com.br/guia-empresas/empresa/CCRO3/proventos"
+        ////"http://fundamentus.com.br/proventos.php?papel=ABEV3&tipo=2";            
+        private const string DIVIDEND_SITE_URI = "https://statusinvest.com.br/acoes/";
+
+
 
         // https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-timer
         // https://codehollow.com/2017/02/azure-functions-time-trigger-cron-cheat-sheet/
-
-
-
-
         [FunctionName("DividendScraperFunction")]
         public static async Task Run([TimerTrigger("0 0 10 * * *")]TimerInfo myTimer, ILogger log)
         {
 
-
-            ////"https://www.bussoladoinvestidor.com.br/guia-empresas/empresa/CCRO3/proventos"
-            ////"http://fundamentus.com.br/proventos.php?papel=ABEV3&tipo=2";            
-
             // TODO scrape all stocks listed on the database and save new dividends to mongodb
 
+
             IEnumerable<Dividend> dividendList =
-                await new DividendListBuilder().ScrapeAndBuildDividendListAsync("https://statusinvest.com.br/acoes/", "ccro3");
+                await new DividendListBuilder().ScrapeAndBuildDividendListAsync(DIVIDEND_SITE_URI, "ccro3");
 
 
 
