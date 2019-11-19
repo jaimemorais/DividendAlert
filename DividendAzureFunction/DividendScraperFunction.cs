9b8728fabd3249635cc049397b7d725d2c6b3d6a
Jaime Morais <jaimemorais@gmail.com>
2019-11-19 14:04:03 -0300

    Create scrape function

--- DividendAzureFunction/DividendScraperFunction.cs
+++ DividendAzureFunction/DividendScraperFunction.cs
@@ -1,6 +1,10 @@
+using DividendAlertData.Model;
+using DividendAlertData.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DividendAzureFunction
 {
@@ -10,14 +14,21 @@ namespace DividendAzureFunction
         // https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-timer
         // https://codehollow.com/2017/02/azure-functions-time-trigger-cron-cheat-sheet/
 
+
+
+
         [FunctionName("DividendScraperFunction")]
-        public static void Run([TimerTrigger("0 0 10 * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 0 10 * * *")]TimerInfo myTimer, ILogger log)
         {
 
-            // TODO scrape all stocks listed on the database and save new dividends to mongodb
+
             ////"https://www.bussoladoinvestidor.com.br/guia-empresas/empresa/CCRO3/proventos"
             ////"http://fundamentus.com.br/proventos.php?papel=ABEV3&tipo=2";            
-            //await _dividendListBuilder.ScrapeAndBuildDividendListAsync("https://statusinvest.com.br/acoes/", "ccro3");
+
+            // TODO scrape all stocks listed on the database and save new dividends to mongodb
+
+            IEnumerable<Dividend> dividendList =
+                await new DividendListBuilder().ScrapeAndBuildDividendListAsync("https://statusinvest.com.br/acoes/", "ccro3");
 
 
 
