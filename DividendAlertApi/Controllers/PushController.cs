1b55ebb5623b6f86e9bcc6c8c31d29ff281fee9a
Jaime Morais <jaimemorais@gmail.com>
2019-11-24 11:26:21 -0300

    Create Push Controller

---
+++ DividendAlertApi/Controllers/PushController.cs
@@ -0,0 +1,22 @@
+﻿using DividendAlertData.MongoDb;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DividendAlertApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PushController : ControllerBase
+    {
+
+        private readonly IDividendRepository _dividendRepository;
+
+
+        public PushController(IDividendRepository dividendRepository)
+        {
+            _dividendRepository = dividendRepository;
+        }
+
+
+
+    }
+}
